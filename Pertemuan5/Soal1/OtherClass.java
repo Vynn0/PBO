0b78788e2f6f0f5815172254ac89a2736230082c
Vynn <121730359+Vynn0@users.noreply.github.com>
2023-10-18 11:36:32 +0700

    Add files via upload

---
+++ Pertemuan5/Soal1/OtherClass.java
@@ -0,0 +1,7 @@
+public class OtherClass {
+    // PBO Pertemuan 5, slide 5.2. Memanggil class lain dalam file lain
+    public static void main(String[] args) {
+        MyClass myObj = new MyClass();
+        System.out.println(myObj.x);
+    }
+}
